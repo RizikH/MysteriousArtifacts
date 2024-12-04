25a98156210b5d2905f478c43ed8302b883dca25
RizikH <rihaddad2@uncg.edu>
2024-12-04 02:33:55 -0500

    Fixed display issues with checkout page

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -1,101 +1,98 @@
-'use strict'
-const shopModel = require('../models/shop.model')
-const userModel = require('../models/users.model')
+'use strict';
+const shopModel = require('../models/shop.model');
+const userModel = require('../models/users.model');
 
 // Redirect to Home Page
 function redirectHome(req, res) {
 	try {
-		const user = req.user ? userModel.getUserById(req.user.id) : null
+		const user = req.user ? userModel.getUserById(req.user.id) : null;
 
 		res.render('index', {
 			title: 'Home - Mysterious Artifacts',
 			user
-		})
+		});
 	} catch (error) {
-		console.error('Error in redirectHome:', error)
-		res.status(500).send('Internal Server Error')
+		console.error('Error in redirectHome:', error);
+		res.status(500).send('Internal Server Error');
 	}
 }
 
 function redirectProducts(req, res) {
 	try {
-		const user = req.user ? userModel.getUserById(req.user.id) : null
-
-		const products = shopModel.getProducts()
+		const user = req.user ? userModel.getUserById(req.user.id) : null;
+		const products = shopModel.getProducts();
 		res.render('products', {
 			products,
 			title: 'Products - Mysterious Artifacts',
 			user // Pass the user object, which might be null
-		})
+		});
 	} catch (error) {
-		console.error('Error in redirectProducts:', error)
-		res.status(500).send('Internal Server Error')
+		console.error('Error in redirectProducts:', error);
+		res.status(500).send('Internal Server Error');
 	}
 }
 
 function sendToCart(req, res) {
 	try {
-		const user = userModel.getUserById(req.user.id)
-		const cart = shopModel.getCart(user.user_id)
-
-		shopModel.sendToCart(cart.cart_id, req.body.product_id)
-
-		res.redirect('/MysteriousArtifacts/cart')
+		const user = userModel.getUserById(req.user.id);
+		const cart = shopModel.getCart(user.user_id);
+		shopModel.sendToCart(cart.cart_id, req.body.product_id);
+		res.redirect('/MysteriousArtifacts/cart');
 	} catch (error) {
-		console.error('Error in sendToCart:', error)
-		res.status(500).send('Internal Server Error')
+		console.error('Error in sendToCart:', error);
+		res.status(500).send('Internal Server Error');
 	}
 }
 
 // Redirect to Cart Page
 function redirectCart(req, res) {
 	try {
-		const user = userModel.getUserById(req.user.id)
-		const cart = shopModel.getCart(user.user_id)
-		const cartProducts = shopModel.getCartProducts(cart.cart_id)
+		const user = userModel.getUserById(req.user.id);
+		const cart = shopModel.getCart(user.user_id);
+		const cartProducts = shopModel.getCartProducts(cart.cart_id);
 		res.render('cart', {
 			cartProducts,
 			title: 'Cart - Mysterious Artifacts',
 			user,
 			cart
-		})
+		});
 	} catch (error) {
-		console.error('Error in redirectCart:', error)
-		res.status(500).send('Internal Server Error')
+		console.error('Error in redirectCart:', error);
+		res.status(500).send('Internal Server Error');
 	}
 }
 
 // Delete a Product from the Cart
 function deleteFromCart(req, res) {
 	try {
-		const user = userModel.getUserById(req.user.id)
-		const cart = shopModel.getCart(user.user_id)
-		shopModel.deleteProduct(cart.cart_id, req.body.productID)
-		res.redirect('/MysteriousArtifacts/cart')
+		const user = userModel.getUserById(req.user.id);
+		const cart = shopModel.getCart(user.user_id);
+		shopModel.deleteProduct(cart.cart_id, req.body.productID);
+		res.redirect('/MysteriousArtifacts/cart');
 	} catch (error) {
-		console.error('Error in deleteFromCart:', error)
-		res.status(500).send('Internal Server Error')
+		console.error('Error in deleteFromCart:', error);
+		res.status(500).send('Internal Server Error');
 	}
 }
 
 function updateCart(req, res) {
-	const user = userModel.getUserById(req.user.id)
-	const cart = shopModel.getCart(user.user_id)
-	const productID = req.body.product_id
-	const quantity = req.body.quantity
-	const action = req.body.action
+	const user = userModel.getUserById(req.user.id);
+	const cart = shopModel.getCart(user.user_id);
+	const productID = req.body.product_id;
+	const quantity = req.body.quantity;
+	const action = req.body.action;
 
 	if (quantity === '1' && action === 'decrease') {
-		shopModel.deleteProduct(cart.cart_id, productID)
-		res.redirect('/MysteriousArtifacts/cart')
+		shopModel.deleteProduct(cart.cart_id, productID);
+		res.redirect('/MysteriousArtifacts/cart');
 	}
 
 	if (req.body.action === 'decrease') {
-		shopModel.minusQuantity(cart.cart_id, productID)
-		res.redirect('/MysteriousArtifacts/cart')
+		shopModel.minusQuantity(cart.cart_id, productID);
+		res.redirect('/MysteriousArtifacts/cart');
 	} else {
-		shopModel.plusQuantity(cart.cart_id, productID)
-		res.redirect('/MysteriousArtifacts/cart')
+		shopModel.plusQuantity(cart.cart_id, productID);
+		res.redirect('/MysteriousArtifacts/cart');
 	}
 }
 
@@ -104,12 +101,11 @@ function checkout(req, res) {
 	const user = userModel.getUserById(cart.user_id);
 	const userId = user.user_id;
 
-
 	const orderCreated = shopModel.createOrder(cart.cart_id, cart.cart_total);
 	const orderId = orderCreated.lastInsertRowid;
 
 	const order = shopModel.getOrder(orderId);
-	console.log("order: ", order);
+
 	shopModel.checkout(cart.cart_id);
 
 	const cartItems = shopModel.getCartProducts(cart.cart_id);
@@ -120,7 +116,7 @@ function checkout(req, res) {
 		cart,
 		user
 	});
- }
+}
 
 module.exports = {
 	redirectHome,
@@ -130,4 +126,4 @@ module.exports = {
 	deleteFromCart,
 	updateCart,
 	checkout
-}
+};
--- models/shop.model.js
+++ models/shop.model.js
@@ -1,176 +1,174 @@
-const db = require('./db-conn')
+const db = require('./db-conn');
 
 // Get all products
 function getProducts() {
-    const sql = `
-    SELECT 
-      product_id AS id, 
-      product_name AS name, 
-      product_description AS description, 
-      product_image_url AS imageUrl, 
-      product_price AS price 
-    FROM products;
-  `
-    return db.all(sql)
+	const sql = `
+        SELECT 
+            product_id AS id, 
+            product_name AS name, 
+            product_description AS description, 
+            product_image_url AS imageUrl, 
+            product_price AS price 
+        FROM products;
+    `;
+	return db.all(sql);
 }
 
 // Get a cart for a user or create a new one if it doesn't exist
 function getCart(userId) {
-    const sql = `
-    SELECT *
-    FROM carts
-    WHERE cart_status = 'new' AND user_id = ?;
-  `
-    const cart = db.get(sql, userId)
-    return cart ? cart : createNewCart(userId)
+	const sql = `
+        SELECT *
+        FROM carts
+        WHERE cart_status = 'new' AND user_id = ?;
+    `;
+	const cart = db.get(sql, userId);
+	return cart ? cart : createNewCart(userId);
 }
 
 // Get cart by cart id
 function getCartById(cartId) {
-    const sql = `
-    SELECT *
-    FROM carts
-    WHERE cart_id = ?;
+	const sql = `
+        SELECT *
+        FROM carts
+        WHERE cart_id = ?;
     `;
-
-    return db.get(sql, cartId)
+	return db.get(sql, cartId);
 }
 
 // Create a new cart
 function createNewCart(userId) {
-    const sql = `
-    INSERT INTO carts(user_id, cart_status)
-    VALUES(?, 'new');
-  `
-    db.run(sql, userId)
-    return getCart(userId) // Return the newly created cart ID
+	const sql = `
+        INSERT INTO carts(user_id, cart_status)
+        VALUES(?, 'new');
+    `;
+	db.run(sql, userId);
+	return getCart(userId); // Return the newly created cart ID
 }
 
 // Get details of a specific product by its ID
 function getProductById(productId) {
-    const sql = `
-    SELECT *
-    FROM products
-    WHERE product_id = ?;
-  `
-    return db.get(sql, productId)
+	const sql = `
+        SELECT *
+        FROM products
+        WHERE product_id = ?;
+    `;
+	return db.get(sql, productId);
 }
 
 // Get all products in a cart
 function getCartProducts(cartId) {
-    const sql = `
-    SELECT 
-      p.product_id, 
-      p.product_name, 
-      p.product_description, 
-      p.product_image_url, 
-      p.product_price, 
-      cp.cart_product_quantity
-    FROM cart_products cp
-    JOIN products p ON cp.product_id = p.product_id
-    WHERE cp.cart_id = ?;
-  `
-    return db.all(sql, cartId)
+	const sql = `
+        SELECT 
+            p.product_id, 
+            p.product_name, 
+            p.product_description, 
+            p.product_image_url, 
+            p.product_price, 
+            cp.cart_product_quantity
+        FROM cart_products cp
+        JOIN products p ON cp.product_id = p.product_id
+        WHERE cp.cart_id = ?;
+    `;
+	return db.all(sql, cartId);
 }
 
 // Check if a product exists in a cart
 function checkIfExists(cartId, productId) {
-    const sql = `
-    SELECT product_id
-    FROM cart_products
-    WHERE cart_id = ? AND product_id = ?;
-  `
-    const product = db.get(sql, cartId, productId)
-    return !!product // Returns true if the product exists, false otherwise
+	const sql = `
+        SELECT product_id
+        FROM cart_products
+        WHERE cart_id = ? AND product_id = ?;
+    `;
+	const product = db.get(sql, cartId, productId);
+	return !!product; // Returns true if the product exists, false otherwise
 }
 
 // Add a product to a cart
 function sendToCart(cartId, productId) {
-    if (checkIfExists(cartId, productId)) {
-        return updateQuantity(cartId, productId, 1) // Increment quantity if the product already exists
-    }
-    const sql = `
-    INSERT INTO cart_products (cart_id, product_id, cart_product_quantity)
-    VALUES (?, ?, 1);
-  `
-    return db.runParams(sql, cartId, productId)
+	if (checkIfExists(cartId, productId)) {
+		return updateQuantity(cartId, productId, 1); // Increment quantity if the product already exists
+	}
+	const sql = `
+        INSERT INTO cart_products (cart_id, product_id, cart_product_quantity)
+        VALUES (?, ?, 1);
+    `;
+	return db.runParams(sql, cartId, productId);
 }
 
 // Increment or decrement product quantity in a cart
 function updateQuantity(cartId, productId, change) {
-    const sql = `
-    UPDATE cart_products
-    SET cart_product_quantity = cart_product_quantity + ?
-    WHERE cart_id = ? AND product_id = ?;
-  `
-    return db.runParams(sql, change, cartId, productId)
+	const sql = `
+        UPDATE cart_products
+        SET cart_product_quantity = cart_product_quantity + ?
+        WHERE cart_id = ? AND product_id = ?;
+    `;
+	return db.runParams(sql, change, cartId, productId);
 }
 
 // Decrease quantity of a product in a cart
 function minusQuantity(cartId, productId) {
-    return updateQuantity(cartId, productId, -1)
+	return updateQuantity(cartId, productId, -1);
 }
 
-// Increase quanitity of a product in a cart
+// Increase quantity of a product in a cart
 function plusQuantity(cartId, productId) {
-    return updateQuantity(cartId, productId, 1)
+	return updateQuantity(cartId, productId, 1);
 }
 
 // Delete a product from a cart
 function deleteProduct(cartId, productId) {
-    if (!checkIfExists(cartId, productId)) {
-        console.error(
-            `Failed to delete: Product with ID = ${productId} not found in cart ID = ${cartId}.`
-        )
-        throw new Error('Error deleting product')
-    }
-    const sql = `
-    DELETE FROM cart_products
-    WHERE cart_id = ? AND product_id = ?;
-  `
-    return db.runParams(sql, cartId, productId)
-}
-
-function createOrder(cartId, orderTotal){
-    const orderStatus = 'new'
-    const sql = `
-    INSERT INTO orders(cart_id, order_total, order_status)
-    VALUES(?, ?, ?);
+	if (!checkIfExists(cartId, productId)) {
+		console.error(
+			`Failed to delete: Product with ID = ${productId} not found in cart ID = ${cartId}.`
+		);
+		throw new Error('Error deleting product');
+	}
+	const sql = `
+        DELETE FROM cart_products
+        WHERE cart_id = ? AND product_id = ?;
     `;
-    
-    return db.runParams(sql, cartId, orderTotal, orderStatus);
+	return db.runParams(sql, cartId, productId);
 }
 
-function checkout(cartId) { 
-    let sql = `
-    UPDATE carts
-    SET cart_status = 'purchased'
-    WHERE cart_id = ?;
+function createOrder(cartId, orderTotal) {
+	const orderStatus = 'new';
+	const sql = `
+        INSERT INTO orders(cart_id, order_total, order_status)
+        VALUES(?, ?, ?);
     `;
-
-    db.run(sql, cartId);
+	return db.runParams(sql, cartId, orderTotal, orderStatus);
 }
 
-function getOrder(orderId){
-    const sql = `
-    SELECT *
-    FROM orders
-    WHERE order_id = ?;`;
+function checkout(cartId) {
+	const sql = `
+        UPDATE carts
+        SET cart_status = 'purchased'
+        WHERE cart_id = ?;
+    `;
+	db.run(sql, cartId);
+}
 
-    return db.run(sql, orderId);
-};
+function getOrder(orderId) {
+	const sql = `
+        SELECT *
+        FROM orders
+        WHERE order_id = ?;
+    `;
+	return db.get(sql, orderId);
+}
+
 module.exports = {
-    getProducts,
-    getCart,
-    getCartById,
-    getCartProducts,
-    sendToCart,
-    createNewCart,
-    minusQuantity,
-    plusQuantity,
-    deleteProduct,
-    getProductById,
-    createOrder,
-    checkout,
-    getOrder
-}
+	getProducts,
+	getCart,
+	getCartById,
+	getCartProducts,
+	sendToCart,
+	createNewCart,
+	minusQuantity,
+	plusQuantity,
+	deleteProduct,
+	getProductById,
+	createOrder,
+	checkout,
+	getOrder
+};
